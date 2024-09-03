ac251e7137d774f7465311197eb7341cd0c70abb
Daniel Mau <daniel.b.mau@gmail.com>
2024-09-03 09:06:43 -0400

    score measure is a bit better

--- game_screen.gd
+++ game_screen.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-var player_hit_time = 0
+var previous_player_hit_time= 0
 var player_hit_time_delta = 0
-var current_time_in_msec = 0
+var current_player_hit_time = 0
 var bass_drum
 var bass_drum_timer
 var bass_drum_timer_goal
@@ -23,8 +23,6 @@ func _ready() -> void:
 
 	bass_drum_timer.wait_time = snapped(float(60.0 / Global.selected_bpm), 0.01)
 	
-	print(bass_drum_timer.wait_time, " ", float(60.0/Global.selected_bpm))
-	
 	bass_drum_timer_goal = bass_drum_timer.wait_time * 1000
 	bass_drum.playing = true
 	bass_drum_timer.start()
@@ -36,22 +34,27 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if game_start == true:
 		if Input.is_action_just_pressed("player_hit"):
-			current_time_in_msec = Time.get_ticks_msec()
-			player_hit_time_delta = current_time_in_msec - player_hit_time
-			player_hit_time = current_time_in_msec
+			print(previous_player_hit_time)
+			current_player_hit_time = Time.get_ticks_msec()
+			print(current_player_hit_time)
+			player_hit_time_delta = current_player_hit_time - previous_player_hit_time
+			previous_player_hit_time= current_player_hit_time
 			score_set.append((player_hit_time_delta * 100.0) / bass_drum_timer_goal )
 			print("Your score: ", player_hit_time_delta)
 
 func _on_bass_drum_timer_timeout() -> void:
 	bass_drum.playing = true
-	
+
 	if four_count > 1:
 		four_count -= 1
 		countdown.text = str(four_count)
+		if four_count == 1:
+			previous_player_hit_time = Time.get_ticks_msec()
+			print(previous_player_hit_time)
 	else:
 		game_start = true
-		countdown.text = ""
 		number_of_beats -= 1
+		countdown.text = ""
 		bass_drum.volume_db = -100
 		if number_of_beats == -1:
 			print(score_set)
