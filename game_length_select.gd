2cd585debdb1489b51e2a857b51e1bd0d42bd9e5
Daniel Mau <daniel.b.mau@gmail.com>
2024-09-03 13:25:16 -0400

    Added functionality to game length select screen

--- game_length_select.gd
+++ game_length_select.gd
@@ -13,3 +13,21 @@ func _process(delta: float) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://bpm_select.tscn")
+
+
+func _on_sixteen_beats_button_pressed() -> void:
+	Global.selected_length = 16
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_twenty_four_beats_button_pressed() -> void:
+	Global.selected_length = 24
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_thirty_two_beats_button_pressed() -> void:
+	Global.selected_length = 32
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_length_input_text_submitted(new_text: String) -> void:
+	Global.selected_length = int(new_text)
+	get_tree().change_scene_to_file("res://game.tscn")
